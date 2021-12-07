19992492a0b0605c26c24139ff40fbaf71362165
Huang Xingqiao <a92304a92304@gmail.com>
2021-12-07 23:13:13 +0800

    fix: specify SpeechSynthesisUtterance property `lang`

--- src/modules/speak.ts
+++ src/modules/speak.ts
@@ -2,6 +2,7 @@ export default function (utterance: SpeechSynthesisUtterance) {
   return new Promise((resolve, reject) => {
     utterance.addEventListener('end', resolve, true)
     utterance.addEventListener('error', reject, true)
+    utterance.lang = 'zh-TW'
     speechSynthesis.speak(utterance)
   })
 }
